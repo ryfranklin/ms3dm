81e52c7fdb399b7c252de5235253fa58fc9e0920
Ryan Franklin <ryan.franklin@ms3dm.tech>
2022-05-22 01:55:33 -0700

    Add front end

---
+++ src/views/IndexView/IndexView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebBasic from 'views/WebBasic';
+
+const IndexView = () => {
+  return (
+    <>
+      <WebBasic />
+    </>
+
+  );
+};
+
+export default IndexView;
